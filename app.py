0dfe4eb3c920b17efb79289bd729696518558622
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2020-02-03 14:45:54 +0100

    add yaml read config

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import yaml
+
 app = Flask(__name__)
+
+config_file = open('config.yaml', 'r')
+config = yaml.load(config_file, Loader=yaml.FullLoader)
 
 @app.route('/')
 def index():
-    return 'Obsługa drukarek etykiet brother'
+    return 'Obsługa drukarek etykiet brother' + config['foo']
