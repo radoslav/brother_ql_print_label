08f4eeb4f6c53054553b10826ced711238aba04e
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2020-02-03 15:03:07 +0100

    config from yaml

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 config_file = open('config.yaml', 'r')
 config = yaml.load(config_file, Loader=yaml.FullLoader)
+BROTHER_QL_PRINTER = config['printer']['model']
+BROTHER_QL_MODEL = config['printer']['connection']
+LABEL_WIDTH = config['printer']['width']
 
 @app.route('/')
 def index():
-    return 'Obsługa drukarek etykiet brother' + config['foo']
+    return 'Obsługa drukarek etykiet brother'
