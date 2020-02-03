fa177af92fab157c0f9b6c0ad44d88181e9e8c3a
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2020-02-03 20:17:49 +0100

    working on img

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-import yaml
 from flask import request
+import yaml
+import qrcode
+from PIL import Image, ImageDraw, ImageFont
 
 app = Flask(__name__)
 
@@ -19,7 +21,23 @@ def index():
 def preview():
 
     req = request.get_json()
-
     print(req)
+
+    qr = qrcode.QRCode(box_size=10)
+    qr.add_data('http://google.pl')
+    qr.make()
+    img_qr = qr.make_image()
+
+    img = Image.new('RGB', (696, 991), color = (255, 255, 255))
+    fnt = ImageFont.truetype('FreeMonoBold.ttf', 60)
+    d = ImageDraw.Draw(img)
+    d.text((10,img_qr.size[1]), "ID: ", font=fnt, fill=(0,0,0))
+
+    d.text((10, 700), "fdfsdfsdf: ", font=fnt, fill=(0,0,0))
+    d.text((10, 800), "fdfsdfsdf: ", font=fnt, fill=(0,0,0))
+
+    img.paste(img_qr, (0,0))
+
+    img.save('./img/pil_text.png')
 
     return "ok", 200
