e6b3ef2b530fe6b0a9240a792d0d48e12067b620
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2020-02-04 22:10:02 +0100

    json preview

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import request
 import yaml
 import qrcode
 from PIL import Image, ImageDraw, ImageFont
+import json
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ config = yaml.load(config_file, Loader=yaml.FullLoader)
 BROTHER_QL_PRINTER = config['printer']['model']
 BROTHER_QL_MODEL = config['printer']['connection']
 LABEL_WIDTH = config['printer']['width']
+
 
 @app.route('/')
 def index():
@@ -23,30 +25,60 @@ def preview():
     req = request.get_json()
     print(req)
 
+    label = jsonToLabel(request.get_json())
+
     qr = qrcode.QRCode(box_size=10)
-    qr.add_data('http://google.pl')
+    qr.add_data(label.url)
     qr.make()
     img_qr = qr.make_image()
 
     fnt = ImageFont.truetype('FreeMonoBold.ttf', 60)
     fnt_bigger = ImageFont.truetype('FreeMonoBold.ttf', 84)
 
-    img_txt = Image.new('RGB', (900, 696), color = (255, 255, 255))
+    img_txt = Image.new('RGB', (900, 696), color=(255, 255, 255))
     d_offset = 60
     d = ImageDraw.Draw(img_txt)
-    d.text((10, d_offset ), "ID: ", font=fnt_bigger, fill=(0,0,0))
-    d.text((10, d_offset + 150), "fdfsdfsdf: ", font=fnt_bigger, fill=(0,0,0))
-    d.text((10, d_offset + 300), "fdfsdfsdf: ", font=fnt_bigger, fill=(0,0,0))
-    d.text((10, d_offset + 450), "fdfsdfsdf: ", font=fnt_bigger, fill=(0,0,0))
+    d.text((10, d_offset), 'id: ' + str(label.id), font=fnt_bigger, fill=(0, 0, 0))
+    d.text((10, d_offset + 150), label.supplier_name,
+           font=fnt_bigger, fill=(0, 0, 0))
+    d.text((10, d_offset + 300), label.print_material_type,
+           font=fnt_bigger, fill=(0, 0, 0))
+    d.text((10, d_offset + 450), label.print_material,
+           font=fnt_bigger, fill=(0, 0, 0))
 
-    img = Image.new('RGB', (696, 1200), color = (255, 255, 255))
+    img = Image.new('RGB', (696, 1200), color=(255, 255, 255))
 
     d_id = ImageDraw.Draw(img)
-    d_id.text((10, 150 ), "ID: ", font=fnt, fill=(0,0,0))
+    d_id.text((40, 50), 'ID: ', font=fnt, fill=(0, 0, 0))
+    d_id.text((40, 150), str(label.id), font=fnt, fill=(0, 0, 0))
 
-    img.paste(img_qr, (350,0))
-    img.paste(img_txt.rotate(-90, expand=1), (0,320))
+    img.paste(img_qr, (350, 0))
+    img.paste(img_txt.rotate(-90, expand=1), (0, 320))
 
     img.save('./img/pil_text.png')
 
     return "ok", 200
+
+def jsonToLabel(json):
+    label = Label
+    label.id = json.get('id')
+    label.supplier_name = json.get('supplier_name')
+    label.print_material_type = json.get('print_material_type')
+    label.print_material = json.get('print_material')
+    label.url = json.get('url')
+    return label
+
+class Label:
+    """
+    Custom Label Class
+    """
+
+    def __init__(self, id, supplier_name, print_material_type, print_material, url):
+        self.id = id
+        self.supplier_name = supplier_name
+        self.print_material_type = print_material_type
+        self.print_material = print_material
+        self.url = url
+
+    def __str__(self):
+        return self.id
