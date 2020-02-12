a5b6dd848b84c6e1485945e83907cc87f5eb6f32
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2020-02-12 18:57:16 +0100

    check if printer is connected

--- app.py
+++ app.py
@@ -124,6 +124,12 @@ def image_to_png_bytes(im):
     image_buffer.seek(0)
     return image_buffer.read()
 
+def is_printer_on(printer):
+    dev = usb.core.find(idVendor=printer.idVendor, idProduct=printer.idProduct)
+    if dev:
+        return True
+    else:
+            return False
 
 app = Flask(__name__)
 
@@ -134,7 +140,7 @@ BACKEND_CLASS = backend_factory(selected_backend)['backend_class']
 
 @app.route('/')
 def index():
-    return 'Obsługa drukarek etykiet brother'
+    return '<h1>Obsługa drukarek etykiet brother</h1> Czy drukarka włączona: ' + str(is_printer_on(printer))
 
 
 @app.route("/api/print", methods=["POST"])
