21bb363a9022aee85237fcd62f9b070ee9fc77a7
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2020-02-24 21:44:45 +0100

    refactor brother_ql

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ import re
 
 from io import BytesIO
 
-from brother_ql.devicedependent import models, label_type_specs, label_sizes
+import brother_ql.models  
+import brother_ql.labels
 # from brother_ql.devicedependent import ENDLESS_LABEL, DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL
 from brother_ql import BrotherQLRaster, create_label
 from brother_ql.backends import backend_factory, guess_backend
