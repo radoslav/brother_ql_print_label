6c05b093f58e8a9ba864b2abc997574d5a549031
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2020-04-28 20:05:12 +0200

    preview check if printer connected

--- app/views.py
+++ app/views.py
@@ -19,20 +19,23 @@ printer = yaml_to_printer()
 selected_backend = guess_backend(printer.connection)
 BACKEND_CLASS = backend_factory(selected_backend)['backend_class']
 
+
 @app.route('/')
 def index():
     q_len = len(q)
     jobs = q.jobs
     return render_template("index.html", jobs=jobs, q_len=q_len)
 
+
 @app.route("/api", methods=["GET"])
 def api():
-
     return "ok", 200
+
 
 @app.route("/api/printer_on", methods=["GET"])
 def printer_on():
     return str(is_printer_on(printer)), 200
+
 
 @app.route("/api/print", methods=["POST"])
 def api_print():
@@ -57,18 +60,22 @@ def api_print():
 
     return return_dict, 200
 
+
 # curl --header "Content-Type: application/json" --request POST --data '[{"id":1463, "supplier_name": "ENDUTEX", "print_material_type": "backlight", "print_material": "Vinyl BP (endutex) niezaciągający wody", "url": "http://192.168.1.100/warehouse_print_materials/1463", "copies":2}]' http://127.0.0.1:5000/api/preview
 @app.route("/api/preview", methods=["POST"])
 def preview():
     app.logger.warning("dsdsds")
     labels = jsonToLabels(request.get_json())
 
-    for i, label in enumerate(labels):
-        img = img_label(label)
-        img.save('./app/img/test_copy_'+str(i)+'.png')
+    if not is_printer_on(printer): # negation for testing
+        for i, label in enumerate(labels):
+            img = img_label(label)
+            img.save('./app/img/test_copy_' + str(i) + '.png')
 
-        task = q.enqueue(print_task, label, description='test')
-        message = f"Task queued at {task.enqueued_at.strftime('%a, %d %b %Y %H:%M:%S')}. {len(q)} jobs queued"
-        print(message)
+            task = q.enqueue(print_task, label, description='test')
+            message = f"Task queued at {task.enqueued_at.strftime('%a, %d %b %Y %H:%M:%S')}. {len(q)} jobs queued"
+            print(message)
 
-    return "ok", 200
+        return "ok", 200
+    else:
+        return "drukarka not found", 404
