5fd3c473a18e9b433a742d50ae00ded9f2904ed2
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2020-05-13 20:16:04 +0200

    add label width and grammage

--- app/models.py
+++ app/models.py
@@ -19,13 +19,16 @@ class Label:
     Custom Label Class
     """
 
-    def __init__(self, id=None, supplier_name=None, print_material_type=None, print_material=None, url=None, copies=None):
+    def __init__(self, id=None, supplier_name=None, print_material_type=None, print_material=None, url=None,
+                 copies=None, width=None, grammage_or_height=None):
         self.id = id
         self.supplier_name = supplier_name
         self.print_material_type = print_material_type
         self.print_material = print_material
         self.url = url
         self.copies = copies
+        self.width = width
+        self.grammage_or_height = grammage_or_height
 
     def __str__(self):
         return self.id
